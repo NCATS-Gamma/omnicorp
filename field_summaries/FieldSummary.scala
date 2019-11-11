5e22a092e4722a3a039f9a66fa7d8d08a9f01e78
Gaurav Vaidya <gaurav@renci.org>
2019-11-11 15:37:24 -0500

    Reformatted source files with Scalafmt.

--- field_summaries/FieldSummary.scala
+++ field_summaries/FieldSummary.scala
@@ -8,78 +8,84 @@ import scala.xml.{Node, Elem, Text}
 import scala.collection.parallel.ForkJoinTaskSupport
 
 /**
- * FieldSummary generates a summary of all the fields used in an XML file.
- */
- object FieldSummary extends App with LazyLogging {
-   val dataDir = new File(args(0))
-   val outDir = args(1)
-   val parallelism = args(2)
+  * FieldSummary generates a summary of all the fields used in an XML file.
+  */
+object FieldSummary extends App with LazyLogging {
+  val dataDir     = new File(args(0))
+  val outDir      = args(1)
+  val parallelism = args(2)
 
-   // Use a fork-join pool to limit the number of parallel processes to
-   // the number of specified nodes.
-   // See https://docs.scala-lang.org/overviews/parallel-collections/configuration.html
-   val forkJoinPool = new ForkJoinPool(parallelism.toInt)
+  // Use a fork-join pool to limit the number of parallel processes to
+  // the number of specified nodes.
+  // See https://docs.scala-lang.org/overviews/parallel-collections/configuration.html
+  val forkJoinPool = new ForkJoinPool(parallelism.toInt)
 
-   // We've been having some problems with setting memory usage in build.sbt, so I'm logging this
-   // so we can figure out when we're messing this up.
-   val MEGABYTE = 1024 * 1024
-   logger.info(f"Free memory available: ${Runtime.getRuntime.freeMemory/MEGABYTE}%.2f MB")
+  // We've been having some problems with setting memory usage in build.sbt, so I'm logging this
+  // so we can figure out when we're messing this up.
+  val MEGABYTE = 1024 * 1024
+  logger.info(f"Free memory available: ${Runtime.getRuntime.freeMemory / MEGABYTE}%.2f MB")
 
-   // Which files do we need to read?
-   val dataFiles = if (dataDir.isFile) List(dataDir)
-   else dataDir.listFiles().filter(_.getName.endsWith(".xml.gz")).toList
+  // Which files do we need to read?
+  val dataFiles =
+    if (dataDir.isFile) List(dataDir)
+    else dataDir.listFiles().filter(_.getName.endsWith(".xml.gz")).toList
 
-   def readXMLFromGZip(file: File): Elem = {
-     val stream = new GZIPInputStream(new FileInputStream(file))
-     val elem = scala.xml.XML.load(stream)
-     stream.close()
-     elem
-   }
+  def readXMLFromGZip(file: File): Elem = {
+    val stream = new GZIPInputStream(new FileInputStream(file))
+    val elem   = scala.xml.XML.load(stream)
+    stream.close()
+    elem
+  }
 
-   /* Some configuration options */
-   /** We expand all possible values for these attributes */
-   val expandAttributeValues = Set(
-     "PubMedPubDate/PubStatus"
-   )
+  /* Some configuration options */
+  /** We expand all possible values for these attributes */
+  val expandAttributeValues = Set("PubMedPubDate/PubStatus")
 
-   def describeElem(el: Node, parentLabel: String = ""): Seq[(String, String)] = {
-     val nodeFullName = parentLabel + "." + el.label
-     val descriptions = el match {
-       case Text(str) => Seq((parentLabel, str))
-       case _ => el.attributes.map(attr => {
-           val attrName = s"${el.label}/${attr.key}"
-           val attrFullName = s"$parentLabel.$attrName"
-           val valueStr = attr.value.map(_.text).mkString(", ")
+  def describeElem(el: Node, parentLabel: String = ""): Seq[(String, String)] = {
+    val nodeFullName = parentLabel + "." + el.label
+    val descriptions = el match {
+      case Text(str) => Seq((parentLabel, str))
+      case _ =>
+        el.attributes
+          .map(attr => {
+            val attrName     = s"${el.label}/${attr.key}"
+            val attrFullName = s"$parentLabel.$attrName"
+            val valueStr     = attr.value.map(_.text).mkString(", ")
 
-           if (expandAttributeValues contains attrName)
-             (s"$attrFullName#$valueStr", valueStr)
-           else
-             (attrFullName, valueStr)
-         }).toSeq
-     }
+            if (expandAttributeValues contains attrName)
+              (s"$attrFullName#$valueStr", valueStr)
+            else
+              (attrFullName, valueStr)
+          })
+          .toSeq
+    }
 
-     return descriptions ++ el.nonEmptyChildren.flatMap(describeElem(_, nodeFullName))
-   }
+    return descriptions ++ el.nonEmptyChildren.flatMap(describeElem(_, nodeFullName))
+  }
 
-   val NANOSECONDS = 1000000000
-   val MILLISECONDS = 1000
+  val NANOSECONDS  = 1000000000
+  val MILLISECONDS = 1000
 
-   dataFiles.foreach { file =>
-     logger.info(s"Began processing $file")
-     val startTime = System.nanoTime()
+  dataFiles.foreach { file =>
+    logger.info(s"Began processing $file")
+    val startTime = System.nanoTime()
 
-     val rootElement = readXMLFromGZip(file)
-     val parallelArticles = rootElement.nonEmptyChildren.par
-     parallelArticles.tasksupport = new ForkJoinTaskSupport(forkJoinPool)
-     val fields = parallelArticles.flatMap(describeElem(_, rootElement.label))
-     val summary = fields.groupBy(_._1).mapValues(_.size)
-     val outStream = new PrintStream(new GZIPOutputStream(new FileOutputStream(new File(s"$outDir/${file.getName}.fields.txt.gz"))))
-     summary.toSeq.seq.sortBy(_._1).foreach(count => outStream.println(s"${count._2}\t${count._1}"))
-     outStream.close()
+    val rootElement      = readXMLFromGZip(file)
+    val parallelArticles = rootElement.nonEmptyChildren.par
+    parallelArticles.tasksupport = new ForkJoinTaskSupport(forkJoinPool)
+    val fields  = parallelArticles.flatMap(describeElem(_, rootElement.label))
+    val summary = fields.groupBy(_._1).mapValues(_.size)
+    val outStream = new PrintStream(
+      new GZIPOutputStream(new FileOutputStream(new File(s"$outDir/${file.getName}.fields.txt.gz")))
+    )
+    summary.toSeq.seq.sortBy(_._1).foreach(count => outStream.println(s"${count._2}\t${count._1}"))
+    outStream.close()
 
-     val timeTaken = (System.nanoTime() - startTime).toDouble/NANOSECONDS
-     val numArticles = parallelArticles.size
-     logger.info(f"Completed processing $numArticles articles from $file in ${timeTaken}%.2f seconds (${(timeTaken/numArticles)*MILLISECONDS}%.5f ms per article).")
-   }
-   forkJoinPool.shutdown()
- }
+    val timeTaken   = (System.nanoTime() - startTime).toDouble / NANOSECONDS
+    val numArticles = parallelArticles.size
+    logger.info(
+      f"Completed processing $numArticles articles from $file in ${timeTaken}%.2f seconds (${(timeTaken / numArticles) * MILLISECONDS}%.5f ms per article)."
+    )
+  }
+  forkJoinPool.shutdown()
+}
--- field_summaries/SumOutputs.scala
+++ field_summaries/SumOutputs.scala
@@ -9,24 +9,25 @@ import scala.xml.{Node, Elem, Text}
 import scala.collection.parallel.ForkJoinTaskSupport
 
 /**
- * SumOutputs sums up generated outputs to provide overall stats.
- */
- object SumOutputs extends App with LazyLogging {
-   val outDir = new File(args(0))
+  * SumOutputs sums up generated outputs to provide overall stats.
+  */
+object SumOutputs extends App with LazyLogging {
+  val outDir = new File(args(0))
 
-   // Which files do we need to read?
-   val outFiles = if (outDir.isFile) List(outDir)
-   else outDir.listFiles().filter(_.getName.endsWith(".fields.txt.gz")).toList
+  // Which files do we need to read?
+  val outFiles =
+    if (outDir.isFile) List(outDir)
+    else outDir.listFiles().filter(_.getName.endsWith(".fields.txt.gz")).toList
 
-   val lines = outFiles.flatMap(file => {
-     val stream = new GZIPInputStream(new FileInputStream(file))
-     Source.fromInputStream(stream, "utf8").getLines
-   })
+  val lines = outFiles.flatMap(file => {
+    val stream = new GZIPInputStream(new FileInputStream(file))
+    Source.fromInputStream(stream, "utf8").getLines
+  })
 
-   val countFormat = """^(\d+)\t(.*)$""".r
-   val counts = lines.map {
-     case countFormat(count, field) => (field, count.toInt)
-     case str => throw new RuntimeException(s"Unable to parse line '${str.toString}'")
-   }
-   counts.groupBy(_._1).mapValues(_.map(_._2).sum).toSeq.sortBy(_._1).foreach(println(_))
- }
+  val countFormat = """^(\d+)\t(.*)$""".r
+  val counts = lines.map {
+    case countFormat(count, field) => (field, count.toInt)
+    case str                       => throw new RuntimeException(s"Unable to parse line '${str.toString}'")
+  }
+  counts.groupBy(_._1).mapValues(_.map(_._2).sum).toSeq.sortBy(_._1).foreach(println(_))
+}
