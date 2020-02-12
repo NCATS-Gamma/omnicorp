237450bea89ab113bcecccc076b48587a9fa57d8
Gaurav Vaidya <gaurav@renci.org>
2020-02-12 13:53:04 -0500

    First stab at testing the output files.

--- src/test/scala/org/renci/chemotext/OmnicorpTests.scala
+++ src/test/scala/org/renci/chemotext/OmnicorpTests.scala
@@ -1,5 +1,6 @@
 package org.renci.chemotext
 
+import scala.io.Source
 import java.io.File
 import java.nio.file.Files
 
@@ -38,11 +39,17 @@ object OmnicorpTests extends TestSuite {
       val tmpFolder        = Files.createTempDirectory("omnicorp-testing").toFile
 
       test("Make sure we can execute Omnicorp on the example file") {
-        val cmdline                  = Seq("sbt", s"""run none "$examplesForTests" "$tmpFolder" 1""")
-        val (status, stdout, stderr) = exec(cmdline)
+        val (status, stdout, stderr) = exec(Seq("sbt", s"""run none "$examplesForTests" "$tmpFolder" 1"""))
+
+        // Make sure that the output file has triples indicating completion.
+        val outputFile = new File(tmpFolder, "examplesForTests.xml.ttl")
+        val outputBuffer = Source.fromFile(outputFile)
+        val output = outputBuffer.getLines().mkString("\n")
+        assert(output contains "")
+        outputBuffer.close()
 
         // Clean up temporary folder.
-        new File(tmpFolder, "examplesForTests.xml.ttl").delete()
+        outputFile.delete()
         tmpFolder.delete()
 
         // Test output and errors.
@@ -60,11 +67,17 @@ object OmnicorpTests extends TestSuite {
       val tmpFolder       = Files.createTempDirectory("omnicorp-testing").toFile
 
       test("Make sure we get an error message on executing Omnicorp on this example file") {
-        val cmdline                  = Seq("sbt", s"""run none "$failedExamples1" "$tmpFolder" 1""")
-        val (status, stdout, stderr) = exec(cmdline)
+        val (status, stdout, stderr) = exec(Seq("sbt", s"""run none "$failedExamples1" "$tmpFolder" 1"""))
+
+        // Make sure that the output file does not have triples indicating completion.
+        val outputFile = new File(tmpFolder, "failedExamples1.xml.ttl")
+        val outputBuffer = Source.fromFile(outputFile)
+        val output = outputBuffer.getLines().mkString("\n")
+        assert(output contains "")
+        outputBuffer.close()
 
         // Clean up temporary folder.
-        new File(tmpFolder, "failedExamples1.xml.ttl").delete()
+        outputFile.delete()
         tmpFolder.delete()
 
         // Test output and errors.
