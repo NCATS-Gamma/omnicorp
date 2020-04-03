a3242298672b32b947e054f0c4944d8739d79f0e
Gaurav Vaidya <gaurav@renci.org>
2020-04-02 20:41:49 -0400

    Added unit tests for ORCID processing.

--- src/test/scala/org/renci/chemotext/PubMedArticleWrapperUnitTests.scala
+++ src/test/scala/org/renci/chemotext/PubMedArticleWrapperUnitTests.scala
@@ -17,44 +17,97 @@ import scala.util.Success
 )
 object PubMedArticleWrapperUnitTests extends TestSuite {
   val tests = Tests {
-    test("#parseDate") {
-      test("Test processing of valid dates") {
-        val datesTested = Seq(
-          (Year.of(2006), <PubDate><Year>2006</Year></PubDate>),
-          (YearMonth.of(2006, 10), <PubDate><Year>2006</Year><Month>Oct</Month></PubDate>),
-          (
-            LocalDate.of(2006, 10, 21),
-            <PubDate><Year>2006</Year><Day>21</Day><Month>Oct</Month></PubDate>
-          ),
-          (Year.of(1998), <PubDate><MedlineDate>1998 Dec-1999 Jan</MedlineDate></PubDate>),
-          (
-            LocalDate.of(2006, 10, 21),
-            <PubDate><Year>2006</Year><Day>21</Day><Month>10</Month></PubDate>
-          ),
-          (Year.of(2016), <PubDate><MedlineDate>Summer 2016</MedlineDate></PubDate>)
-        )
-
-        datesTested.foreach({
-          case (expected, xmlNode) =>
-            assert(PubMedArticleWrapper.parseDate(xmlNode) == Success(expected))
-        })
+    test("AuthorWrapper") {
+      test("#orcIds") {
+        test("ORCID as a plain number") {
+          val orcids = new AuthorWrapper(<Author>
+            <Identifier Source="ORCID">0000000305870454</Identifier>
+          </Author>).orcIds
+          println(orcids)
+          assert(orcids == Seq("https://orcid.org/0000-0003-0587-0454"))
+        }
+        test("ORCID as a hyphenated number") {
+          val orcids = new AuthorWrapper(<Author>
+            <Identifier Source="ORCID">0000-0003-0587-0454</Identifier>
+          </Author>).orcIds
+          assert(orcids == Seq("https://orcid.org/0000-0003-0587-0454"))
+        }
+        test("ORCID with embedded spaces") {
+          val orcids = new AuthorWrapper(<Author>
+            <Identifier Source="ORCID">0000 0003 0587 0454</Identifier>
+          </Author>).orcIds
+          assert(orcids == Seq("https://orcid.org/0000-0003-0587-0454"))
+        }
+        test("ORCID with spaces instead of hyphens") {
+          val orcids = new AuthorWrapper(<Author>
+            <Identifier Source="ORCID">0000-0003 0587-0454</Identifier>
+          </Author>).orcIds
+          assert(orcids == Seq("https://orcid.org/0000-0003-0587-0454"))
+        }
+        test("ORCID with HTTP URL") {
+          val orcids = new AuthorWrapper(<Author>
+            <Identifier Source="ORCID">http://orcid.org/0000-0003-0587-0454</Identifier>
+          </Author>).orcIds
+          assert(orcids == Seq("https://orcid.org/0000-0003-0587-0454"))
+        }
       }
-      test("Test processing of invalid dates") {
-        assert(
-          (intercept[IllegalArgumentException] {
-            PubMedArticleWrapper
-              .parseDate(<PubDate><MedlineDate>199 Dec-199 Jan</MedlineDate></PubDate>)
-              .get
-          }).getMessage == "Could not parse XML node as date: <PubDate><MedlineDate>199 Dec-199 Jan</MedlineDate></PubDate>"
-        )
-        assert((intercept[IllegalArgumentException] {
-          PubMedArticleWrapper.parseDate(<PubDate></PubDate>).get
-        }).getMessage == "Could not parse XML node as date: <PubDate></PubDate>")
-        assert(
-          (intercept[RuntimeException] {
-            PubMedArticleWrapper.parseDate(<PubDate><Year>2019</Year><Day>12</Day></PubDate>).get
-          }).getMessage == "Could not extract month from node: <PubDate><Year>2019</Year><Day>12</Day></PubDate>"
-        )
+    }
+    test("PubMedArticleWrapper") {
+      test("#parseDate") {
+        test("Test processing of valid dates") {
+          val datesTested = Seq(
+            (Year.of(2006), <PubDate>
+              <Year>2006</Year>
+            </PubDate>),
+            (YearMonth.of(2006, 10), <PubDate>
+              <Year>2006</Year> <Month>Oct</Month>
+            </PubDate>),
+            (
+              LocalDate.of(2006, 10, 21),
+              <PubDate>
+                <Year>2006</Year> <Day>21</Day> <Month>Oct</Month>
+              </PubDate>
+            ),
+            (Year.of(1998), <PubDate>
+              <MedlineDate>1998 Dec-1999 Jan</MedlineDate>
+            </PubDate>),
+            (
+              LocalDate.of(2006, 10, 21),
+              <PubDate>
+                <Year>2006</Year> <Day>21</Day> <Month>10</Month>
+              </PubDate>
+            ),
+            (Year.of(2016), <PubDate>
+              <MedlineDate>Summer 2016</MedlineDate>
+            </PubDate>)
+          )
+
+          datesTested.foreach({
+            case (expected, xmlNode) =>
+              assert(PubMedArticleWrapper.parseDate(xmlNode) == Success(expected))
+          })
+        }
+        test("Test processing of invalid dates") {
+          assert(
+            (intercept[IllegalArgumentException] {
+              PubMedArticleWrapper
+                .parseDate(<PubDate>
+                  <MedlineDate>199 Dec-199 Jan</MedlineDate>
+                </PubDate>)
+                .get
+            }).getMessage matches "Could not parse XML node as date: <PubDate>\\s*<MedlineDate>199 Dec-199 Jan</MedlineDate>\\s*</PubDate>"
+          )
+          assert((intercept[IllegalArgumentException] {
+            PubMedArticleWrapper.parseDate(<PubDate></PubDate>).get
+          }).getMessage == "Could not parse XML node as date: <PubDate></PubDate>")
+          assert(
+            (intercept[RuntimeException] {
+              PubMedArticleWrapper.parseDate(<PubDate>
+                <Year>2019</Year> <Day>12</Day>
+              </PubDate>).get
+            }).getMessage matches "Could not extract month from node: <PubDate>\\s*<Year>2019</Year> <Day>12</Day>\\s*</PubDate>"
+          )
+        }
       }
     }
   }
