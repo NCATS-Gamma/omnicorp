f9221b0ff55b03c66f2f7d536fdeb6eb69120d6a
Gaurav Vaidya <gaurav@renci.org>
2025-01-03 16:32:14 -0500

    Improved code.

--- src/pubtator3/pubtator3_loader.py
+++ src/pubtator3/pubtator3_loader.py
@@ -6,6 +6,7 @@
 # - Loading them into a DuckDB database so that they can be normalized, analyzed and cleaned.
 
 import ftplib
+import os
 import urllib.parse
 import logging
 
@@ -33,21 +34,41 @@ def download_pubtator3(to_dir: str = ".",
 
     # Open an FTP connection.
     conn = ftplib.FTP(host=ftp_url.hostname, user=ftp_username, passwd=ftp_password)
-    conn.login()
     conn.cwd(ftp_url.path)
 
+    # Create directory if not present.
+    os.makedirs(to_dir, exist_ok=True)
+    logging.debug(f"Created directory {to_dir}.")
+
     # Get the list of files to download.
-    files = conn.mlsd()
-    filenames_to_download = list(map(lambda x: x[0], filter(lambda x: xml_only and "XML" in x[0], files)))
+    files = conn.mlsd('', ['size'])
+    download_count = 0
+    for file in files:
+        filename = file[0]
+        if xml_only and "XML" not in file[0]:
+            continue
+
+        download_count += 1
+
+        file_size = file[1]['size']
+
+        logging.info(f"Downloading {filename} ({file_size} bytes)")
+        local_file_path = f"{to_dir}/{filename}"
+        with open(local_file_path, "wb") as f:
+            conn.retrbinary(f"RETR {filename}", f.write)
+    
+        local_file_size = os.path.getsize(local_file_path)
+        logging.info(f"Downloaded {filename} ({local_file_size} bytes on disk)")
+
+    logging.info(f"Downloaded {download_count} files.")
 
 
 @click.group()
 def pubtator3_loader():
     pass
 
-
 @pubtator3_loader.command()
-@click.option("--to-dir", "to_dir", type=click.Path(dir_okay=True, file_okay=False), default=".",
+@click.option("--to", "to_dir", type=click.Path(dir_okay=True, file_okay=False), default=".",
               help="Directory to download PubTator3 files to.")
 @click.option("--pubtator3-ftp-url", type=str, default="ftp://ftp.ncbi.nlm.nih.gov/pub/lu/PubTator3/",)
 @click.option("--ftp-username", type=str, default="anonymous",
@@ -58,12 +79,13 @@ def download(to_dir, pubtator3_ftp_url, ftp_username, ftp_password):
     """
     Download PubTator3 files from pubtator3_ftp_url to to_dir.
     """
+    logging.basicConfig(level=logging.INFO)
 
+    logging.info(f"Downloading PubTator3 from {pubtator3_ftp_url} into {to_dir}.")
     download_pubtator3(to_dir=to_dir,
                        pubtator3_ftp_url=pubtator3_ftp_url,
                        ftp_username=ftp_username,
                        ftp_password=ftp_password)
-    logging.info(f"Loading PubTator3 from {pubtator3_ftp_url} into {to_dir}.")
     pass
 
 
